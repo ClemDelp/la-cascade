d4b7889cc0a9d2c9754dc4fba567f7f2b88e4487
ClemDelp <clement.delpuech@snips.ai>
2020-02-16 11:41:54 +0100

    Updates

---
+++ precache-manifest.492418068ded2d76fdee6704798e1d52.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10821acbcfc9c250859302b3c90f18f1",
+    "url": "/la-cascade/index.html"
+  },
+  {
+    "revision": "1e0b28a49f5a2e10d8f7",
+    "url": "/la-cascade/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "7c55e3e669183575739a",
+    "url": "/la-cascade/static/js/2.add14015.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/la-cascade/static/js/2.add14015.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e0b28a49f5a2e10d8f7",
+    "url": "/la-cascade/static/js/main.8463a52b.chunk.js"
+  },
+  {
+    "revision": "9b22baa0950dd764f4c5",
+    "url": "/la-cascade/static/js/runtime-main.049d68ee.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/la-cascade/static/media/logo.5d5d9eef.svg"
+  }
+]);
